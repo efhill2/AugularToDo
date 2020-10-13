50d3b74d491999bb93723988867a1316a4ed9a37
Eugene Hill <eugenehill@quickenloans.com>
2020-10-13 08:29:28 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularToDo';
+ 
+
+  list: Todo [] = [
+    {task: 'Take out the trash', completed: false},
+    {task: 'Wash the dishes', completed: false},
+    {task: 'Vacuum', completed: false},
+    {task: 'Stretch', completed: true},
+    {task: 'Eat breakfast', completed: true},
+    {task: 'Workout', completed: false},
+    {task: 'Finish MVP', completed: false},
+    {task: 'Cook Dinner', completed: true},
+
+
+  ];
+  // CompleteTask = function() {
+      
+  //     this.list.task.completed == true;
+
+         
+  //     //this.list.task.completed = true;
+  //     // let q = document.getElementById('task')
+  //     // q.id = 'done';
+      
+    
+    
+  // }
+
+  AddToDo = function(newtodo: string) {
+      if(newtodo) {
+        this.list.push({task:newtodo, completed: false})
+      }
+  }
+
 }
+
+interface Todo {
+  task : String,
+  completed : Boolean;
+};
+
+
